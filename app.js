4ddc0d4bc2033c4dda167743257def834c4d9a90
demizz <imedbenazzouna@gmail.com>
2020-08-30 21:33:52 +0100

    modifie the cors

--- app.js
+++ app.js
@@ -38,19 +38,20 @@ mongoose
     console.log(err);
   });
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-  next();
-});
+app.use("*", cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin",
+//     "X-Requested-With",
+//     "Content-Type",
+//     "Accept",
+//     "Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+//   next();
+// });
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 app.use((req, res, next) => {

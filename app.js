04d412bab17cfe567085935a858ff85d125dc001
demizz <imedbenazzouna@gmail.com>
2020-09-08 18:54:05 +0100

    add secure cookies

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/user-routes");
-const HttpError = require("./models/http-error");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/user-routes');
+const HttpError = require('./models/http-error');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-const mongoose = require("mongoose");
-const conf = dotenv.config({ path: "./config.env" });
+const mongoose = require('mongoose');
+const conf = dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
+	'<password>',
+	process.env.DATABASE_PASSWORD
 );
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('public'));
 }
 
 mongoose
-  .connect(DB, {
-    useCreateIndex: true,
-    useFindAndModify: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((con) => {
-    console.log("connection to database successfully");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect(DB, {
+		useCreateIndex: true,
+		useFindAndModify: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((con) => {
+		console.log('connection to database successfully');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 app.use(cors());
-app.use("*", cors());
+app.use('*', cors());
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
@@ -52,28 +52,34 @@ app.use("*", cors());
 //   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
 //   next();
 // });
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
 app.use((req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
-app.use("*", (req, res, next) => {
-  return next(new HttpError("Could not found this route", 404));
+app.use('*', (req, res, next) => {
+	return next(new HttpError('Could not found this route', 404));
 });
 app.use((err, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
-  }
-  if (res.headerSent) {
-    return next(err);
-  }
-  res.status(err.code || 500).json({
-    message: err.message || "An unknown err occurred",
-  });
+	if (req.file) {
+		fs.unlink(req.file.path, (err) => {
+			console.log(err);
+		});
+	}
+	if (res.headerSent) {
+		return next(err);
+	}
+	res.status(err.code || 500).json({
+		message: err.message || 'An unknown err occurred',
+	});
 });
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`server start`);
+const server = app.listen(process.env.PORT || 8000, () => {
+	console.log(`server start`);
+});
+process.on('SIGTERM', () => {
+	console.log('SIGTERM received shut down');
+	server.close(() => {
+		console.log('process finish');
+	});
 });
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,136 +1,138 @@
-const HttpError = require("../models/http-error");
-const { validationResult, cookie } = require("express-validator");
-const User = require("../models/user");
-const catchAsync = require("../util/catchAsync");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const HttpError = require('../models/http-error');
+const { validationResult, cookie } = require('express-validator');
+const User = require('../models/user');
+const catchAsync = require('../util/catchAsync');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getAllUsers = catchAsync(async (req, res, next) => {
-  const allUsers = await User.find({}, "-password");
-  if (!allUsers) {
-    return next(new HttpError("something went wrong please try again", 500));
-  }
-  res.status(200).json({
-    status: "success",
-    message: "list of all users",
-    users: allUsers,
-  });
+	const allUsers = await User.find({}, '-password');
+	if (!allUsers) {
+		return next(new HttpError('something went wrong please try again', 500));
+	}
+	res.status(200).json({
+		status: 'success',
+		message: 'list of all users',
+		users: allUsers,
+	});
 });
 
 const signup = catchAsync(async (req, res, next) => {
-  const { name, email, password } = req.body;
+	const { name, email, password } = req.body;
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new HttpError("Invalid inputs passed, please check your data", 422)
-    );
-  }
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return next(new HttpError("this user already exist,try to login", 422));
-  }
-  let hashedPassword;
-  hashedPassword = await bcrypt.hash(password, 12);
-  if (!hashedPassword) {
-    return next(
-      new HttpError("fail to hash the password please try again", 500)
-    );
-  }
-  const newUser = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-    image: req.file.path,
-    places: [],
-  });
-  if (!newUser) {
-    return next(
-      new HttpError("something went wrong could not create a new user", 500)
-    );
-  }
-  let token;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return next(
+			new HttpError('Invalid inputs passed, please check your data', 422)
+		);
+	}
+	const existingUser = await User.findOne({ email });
+	if (existingUser) {
+		return next(new HttpError('this user already exist,try to login', 422));
+	}
+	let hashedPassword;
+	hashedPassword = await bcrypt.hash(password, 12);
+	if (!hashedPassword) {
+		return next(
+			new HttpError('fail to hash the password please try again', 500)
+		);
+	}
+	const newUser = await User.create({
+		name,
+		email,
+		password: hashedPassword,
+		image: req.file.path,
+		places: [],
+	});
+	if (!newUser) {
+		return next(
+			new HttpError('something went wrong could not create a new user', 500)
+		);
+	}
+	let token;
 
-  try {
-    token = jwt.sign(
-      { userId: newUser._id, email: newUser.email },
-      process.env.SECRET,
-      {
-        expiresIn: process.env.TOKEN_EXPIRES,
-      }
-    );
-  } catch (err) {
-    return next(new HttpError("fail to create the token", 500));
-  }
-  const cookieOption = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-  if (process.env.NODE_ENV === "production") cookieOption.secure = true;
-  res.cookie("JWT", token, cookieOption);
-  res.status(201).json({
-    status: "success",
-    message: "new user created successfully",
-    user: newUser,
-    userId: newUser._id,
-    token,
-  });
-  req.currentUser = newUser._id;
+	try {
+		token = jwt.sign(
+			{ userId: newUser._id, email: newUser.email },
+			process.env.SECRET,
+			{
+				expiresIn: process.env.TOKEN_EXPIRES,
+			}
+		);
+	} catch (err) {
+		return next(new HttpError('fail to create the token', 500));
+	}
+	const cookieOption = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+		secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+	};
+	if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+	res.cookie('JWT', token, cookieOption);
+	res.status(201).json({
+		status: 'success',
+		message: 'new user created successfully',
+		user: newUser,
+		userId: newUser._id,
+		token,
+	});
+	req.currentUser = newUser._id;
 });
 
 const login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
+	const existingUser = await User.findOne({ email });
 
-  let isValidPassword = false;
-  isValidPassword = await bcrypt.compare(password, existingUser.password);
-  if (!existingUser || !isValidPassword) {
-    return next(new HttpError("email or password wrong please try again", 422));
-  }
-  let token;
-  try {
-    token = jwt.sign(
-      { userId: existingUser._id, email: existingUser.email },
-      process.env.SECRET,
-      {
-        expiresIn: process.env.TOKEN_EXPIRES,
-      }
-    );
-  } catch (err) {
-    return next(new HttpError("fail to create the token", 500));
-  }
-  const cookieOption = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-  if (process.env.NODE_ENV === "production") cookieOption.secure = true;
-  res.cookie("JWT", token, cookieOption);
+	let isValidPassword = false;
+	isValidPassword = await bcrypt.compare(password, existingUser.password);
+	if (!existingUser || !isValidPassword) {
+		return next(new HttpError('email or password wrong please try again', 422));
+	}
+	let token;
+	try {
+		token = jwt.sign(
+			{ userId: existingUser._id, email: existingUser.email },
+			process.env.SECRET,
+			{
+				expiresIn: process.env.TOKEN_EXPIRES,
+			}
+		);
+	} catch (err) {
+		return next(new HttpError('fail to create the token', 500));
+	}
+	const cookieOption = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+		secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+	};
+	if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+	res.cookie('JWT', token, cookieOption);
 
-  res.status(200).json({
-    status: "success",
-    message: "logged In",
-    userId: existingUser._id,
-    user: existingUser,
+	res.status(200).json({
+		status: 'success',
+		message: 'logged In',
+		userId: existingUser._id,
+		user: existingUser,
 
-    token,
-  });
+		token,
+	});
 
-  next();
+	next();
 });
 const logout = (req, res, next) => {
-  res.cookie("JWT", "logged out", {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
-  res.status(200).json({
-    status: "success",
-    message: "user logout",
-  });
+	res.cookie('JWT', 'logged out', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+	res.status(200).json({
+		status: 'success',
+		message: 'user logout',
+	});
 };
 exports.logout = logout;
 
